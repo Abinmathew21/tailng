afc5a7b0a455ab1985cb803344193d2369ffa5f5
Tociva Technologies <prince.francis@tociva.com>
2026-01-26 21:51:41 +0530

    Overview and API section of text-input

--- apps/docs/src/app/pages/components/forms/text-input/api/api.component.ts
+++ apps/docs/src/app/pages/components/forms/text-input/api/api.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { TailngBadgeComponent } from '@tociva/tailng-ui';
 
 @Component({
   standalone: true,
   selector: 'docs-text-input-api',
   templateUrl: './api.component.html',
+  imports: [
+    TailngBadgeComponent
+  ],
 })
 export class TextInputApiComponent {}
--- apps/docs/src/app/pages/components/forms/text-input/overview/overview.component.ts
+++ apps/docs/src/app/pages/components/forms/text-input/overview/overview.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { TailngIconComponent } from '@tociva/tailng-icons';
+import { TailngBadgeComponent, TailngTextInputComponent } from '@tociva/tailng-ui';
 
 @Component({
   standalone: true,
   selector: 'docs-text-input-overview',
   templateUrl: './overview.component.html',
+  imports: [
+    TailngIconComponent,
+    TailngTextInputComponent,
+    TailngBadgeComponent
+    ],
 })
 export class TextInputOverviewComponent {}
