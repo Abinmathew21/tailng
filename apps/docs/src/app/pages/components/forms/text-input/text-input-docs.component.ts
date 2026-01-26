8dbf471298a911422302d1025e596838687def5e
Tociva Technologies <prince.francis@tociva.com>
2026-01-26 20:25:13 +0530

    Tabs styles for text-input doc page

--- apps/docs/src/app/pages/components/forms/text-input/text-input-docs.component.ts
+++ apps/docs/src/app/pages/components/forms/text-input/text-input-docs.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  TailngTabsComponent,
+  TailngTabComponent,
+  TailngTabPanelComponent,
+} from '@tociva/tailng-ui';
 
 @Component({
   standalone: true,
   selector: 'docs-text-input',
   templateUrl: './text-input-docs.component.html',
+  imports: [TailngTabsComponent, TailngTabComponent, TailngTabPanelComponent],
 })
 export class TextInputDocsComponent {}
