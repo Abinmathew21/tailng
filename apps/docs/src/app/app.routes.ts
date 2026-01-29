bf2d9a60c4e6119f220dbc3640155746020e37f1
Tociva Technologies <prince.francis@tociva.com>
2026-01-29 19:43:50 +0530

    Getting started pages - routing issue fixed.

--- apps/docs/src/app/app.routes.ts
+++ apps/docs/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { homeRoutes } from './routes/home.routes';
-import { installationRoutes } from './routes/installation.routes';
 import { componentsRoutes } from './routes/components.routes';
 
 export const routes: Routes = [
   ...homeRoutes,
-  ...installationRoutes,
   ...componentsRoutes,
   { path: '**', redirectTo: '' },
 ];
