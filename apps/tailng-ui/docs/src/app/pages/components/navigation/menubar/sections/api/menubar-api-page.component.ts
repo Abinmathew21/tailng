8e3c39389cd15700b608bb14c48b82b8b1d37747
Tociva Technologies <prince.francis@tociva.com>
2026-04-17 22:35:10 +0530

    menubar docs styles refactored.

--- apps/tailng-ui/docs/src/app/pages/components/navigation/menubar/sections/api/menubar-api-page.component.ts
+++ apps/tailng-ui/docs/src/app/pages/components/navigation/menubar/sections/api/menubar-api-page.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menubar-api-page',
   templateUrl: './menubar-api-page.component.html',
-  styleUrls: ['./menubar-api-page.component.css'],
 })
 export class MenubarApiPageComponent {}
